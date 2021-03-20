453e5f4a8096c99fc1b99f120399cfb37b74b18d
artistorado <singharvi090@gmail.com>
2021-03-20 14:37:25 +0530

    update address

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 // eslint-disable-next-line import/prefer-default-export
 export const UniswapV2Router02 = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
-export const DollarPool1 = '0x409c4afa2dcb2c858910ee3fe4a89bb0fd1b56ab';
-export const DollarPool2 = '0x409c4afa2dcb2c858910ee3fe4a89bb0fd1b56ab';
-export const DollarPool3 = '0x409c4afa2dcb2c858910ee3fe4a89bb0fd1b56ab';
-export const DollarPool4 = '0x409c4afa2dcb2c858910ee3fe4a89bb0fd1b56ab';
+export const DollarPool1 = '0x535907c69f149c44257c95948c7356ff294e4eb6';
+export const DollarPool2 = '0x535907c69f149c44257c95948c7356ff294e4eb6';
+export const DollarPool3 = '0x535907c69f149c44257c95948c7356ff294e4eb6';
+export const DollarPool4 = '0x535907c69f149c44257c95948c7356ff294e4eb6';
--- src/constants/tokens.js
+++ src/constants/tokens.js
@@ -1,5 +1,5 @@
 export const UNI = {
-  addr: "0x64dD0d5A7b6DB2801Eb02645600323e270aa73d6",
+  addr: "0x80c6a58b82cf1a8aba84e331960fae19876d035d",
   decimals: 18,
   symbol: "UNI",
 };
@@ -11,13 +11,13 @@ export const USDC = {
 };
 
 export const CLP = {
-  addr: "0x3e40a9eb3350e3d4e7a474560ed88ca51a4bfb57",
+  addr: "0xAf575d793efE7b479030BD4e32e69648293b5792",
   decimals: 18,
   symbol: "CLP",
 };
 
 export const CLPS = {
-  addr: "0x1ca84582DA2eed4990945358954820c13BBf96Da",
+  addr: "0xfD90fe23C8Ac9E52ebd1De91c810d81A657aBC39",
   decimals: 18,
   symbol: "CLPS",
 };
